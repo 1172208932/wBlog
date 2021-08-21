c68036d6c9610a330ffdb32cb7ed9cd8f60cac1d
张晓旭 <zhangxiaoxu@duiba.com.cn>
2021-08-22 00:18:37 +0800

    feat: first commig

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,38 @@
+module.exports = {
+    title: 'Hello 晓旭',
+    description: 'Just Text',
+    head: [
+        ['link', {
+            rel: 'icon',
+            href: 'https://img0.baidu.com/it/u=3041595518,152570800&fm=26&fmt=auto&gp=0.jpg'
+        }]
+    ],
+    themeConfig: {
+        logo: 'https://img0.baidu.com/it/u=3041595518,152570800&fm=26&fmt=auto&gp=0.jpg',
+        nav: [{
+                text: 'HOME',
+                link: '/'
+            },
+            {
+                text: 'WEB',
+                link: '/web/', 
+            },
+            {
+                text: 'GAME',
+                link: '/game/', 
+            },
+            {
+                text: 'GitHub',
+                link: 'https://google.com'
+            },
+        ],
+        sidebar: {
+            '/web/': [
+                '',
+                '1e'
+            ]
+        }
+
+    },
+
+}
---
+++ docs/README.md
@@ -0,0 +1,4 @@
+# Hello VuePress
+<div align="center">
+<img src="https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=2989782649,1389878803&fm=58&bpow=1200&bpoh=1600"width="180" height="200" alt="陈奕迅"></img>
+</div>
